1cd19353dd115f3c056c8d8568bef8549517b63e
liam <liam@marshan.co.za>
2020-06-26 17:09:05 +0200

    added a working db connection unlike you bishes

--- matcha-backend/init.js
+++ matcha-backend/init.js
@@ -1,41 +1,73 @@
 var mysql = require('mysql');
+var users = require('./tables/users.json');
+var debug = false;
 
-var con = mysql.createConnection({
+var user = {
 	host: "localhost",
+	port: "3306",
 	user: "matcha",
-	password: "matcha",
-});
-
-con.connect(function(err) {
-	if (err) throw err;
-	console.log("Connect");
-	var sql = "DROP DATABASE IF EXISTS matcha_db";
-	con.query(sql,function(err,result) {
-		if (err) throw err;
-		// console.log(result);
-	})
-	sql = "CREATE DATABASE IF NOT EXISTS matcha_db"
-	con.query(sql, function(err,result) {
-		if (err) throw err;
-		// console.log(result);
-	})
-});
-
-con = mysql.createConnection({
+	password: "matcha"
+};
+var userDB = {
 	host: "localhost",
+	port: "3306",
 	user: "matcha",
 	password: "matcha",
 	database: "matcha_db"
-});
+};
+
+var con = mysql.createConnection(user)
+if (mysql.createConnection(userDB)){
+	con = mysql.createConnection(userDB);
+	con.connect(function(err) {
+		if (err) { throw err; }
+		console.log("Starting The Endless Journey");
+		var sql = "DROP DATABASE matcha_db";
+		con.query(sql,function(err,result) {
+			if (err) throw err;
+			if (debug) {console.log(result);}
+			console.log("Destroyed The Endless Horizon");
+			return;
+		})
+		return;
+	});
+	con = mysql.createConnection(user);
+}
 
 con.connect(function(err) {
-	if (err) throw err;
-	console.log("Table creation");
-	var sql = "CREATE TABLE ? (?)";
-	var name = "users";
-	var values = "userid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255)";
-	con.query(sql,[name, values], function(err,res){
+	if (err) { throw err; }
+	sql = "CREATE DATABASE matcha_db"
+	con.query(sql, function(err,result) {
 		if (err) throw err;
-		console.log("TABLE CREATED MWAHAHAHA");
-	});
-})
+		if (debug) {console.log(result);}
+		console.log("Found A New Endless Horizon");
+		var conDB = mysql.createConnection(userDB)
+		conDB.connect(function(err) {
+			if (err) { throw err; }
+			console.log("Started A New Endless Loop");
+			var sql = "CREATE TABLE ";
+			var Tablename = "users ";
+			var values = "("+
+			users.userid+","+
+			users.username+","+
+			users.name+","+
+			users.surname+","+
+			users.age+","+
+			users.gender+","+
+			users.email+","+
+			users.password+","+
+			users.sp+","+
+			users.bio+","+
+			users.interests
+			+")";
+			conDB.query(sql+Tablename+values, function (err, result) {
+			  if (err) throw err;
+			  console.log("Completed The New Endless Loop");
+			  return;
+			});
+			return;
+		});
+		return;
+	})
+	return;
+});
--- matcha-frontend/src/index.js
+++ matcha-frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/default.css';
-import Navbar from './narvbar.js'
+//import Navbar from './narvbar.js'
 import Login from './login';
 import SignUp from './sign-up.js';
 
