8bce554a0885e480a7470fded3a07f960be0601c
liam <liam@marshan.co.za>
2020-06-27 13:22:49 +0200

    Feeding The Children With a Teaspoon

--- matcha-backend/init.js
+++ matcha-backend/init.js
@@ -1,5 +1,9 @@
 var mysql = require('mysql');
 var users = require('./tables/users.json');
+var images = require('./tables/images.json');
+var status = require('./tables/status.json');
+var location = require('./tables/location.json');
+var chats = require('./tables/chats.json');
 var debug = false;
 
 var user = {
@@ -21,6 +25,14 @@ if (mysql.createConnection(userDB)){
 	con = mysql.createConnection(userDB);
 	con.connect(function(err) {
 		if (err) { throw err; }
+		console.log("Starting The Endless Journey");
+		var sql = "CREATE DATABASE IF NOT EXISTS matcha_db";
+		con.query(sql,function(err,result) {
+			if (err) throw err;
+			if (debug) {console.log(result);}
+			console.log("Created A New Endless Horizon");
+			return;
+		})
 		console.log("Starting The Endless Journey");
 		var sql = "DROP DATABASE matcha_db";
 		con.query(sql,function(err,result) {
@@ -47,9 +59,13 @@ con.connect(function(err) {
 	return;
 });
 
+
 function createTables(){
 	createUserTable();
-	//createImageTable();
+	createImageTable();
+	createStatusTable();
+	createLocationTable();
+	createChatsTable();
 }
 
 function createUserTable(){
@@ -87,19 +103,78 @@ function createImageTable(){
 		if (err) { throw err; }
 		console.log("Started A New Endless Loop");
 		var sql = "CREATE TABLE ";
-		var Tablename = "users ";
+		var Tablename = "images ";
 		var values = "("+
-		users.userid+","+
-		users.username+","+
-		users.name+","+
-		users.surname+","+
-		users.age+","+
-		users.gender+","+
-		users.email+","+
-		users.password+","+
-		users.sp+","+
-		users.bio+","+
-		users.interests
+		images.userid+","+
+		images.image1+","+
+		images.image2+","+
+		images.image3+","+
+		images.image4+","+
+		images.image5
+		+")";
+		conDB.query(sql+Tablename+values, function (err, result) {
+		  if (err) throw err;
+		  console.log("Completed The New Endless Loop");
+		  return;
+		});
+		return;
+	});
+}
+
+function createStatusTable(){
+	var conDB = mysql.createConnection(userDB)
+	conDB.connect(function(err) {
+		if (err) { throw err; }
+		console.log("Started A New Endless Loop");
+		var sql = "CREATE TABLE ";
+		var Tablename = "status ";
+		var values = "("+
+		status.userid+","+
+		status.online
+		+")";
+		conDB.query(sql+Tablename+values, function (err, result) {
+		  if (err) throw err;
+		  console.log("Completed The New Endless Loop");
+		  return;
+		});
+		return;
+	});
+}
+
+function createLocationTable(){
+	var conDB = mysql.createConnection(userDB)
+	conDB.connect(function(err) {
+		if (err) { throw err; }
+		console.log("Started A New Endless Loop");
+		var sql = "CREATE TABLE ";
+		var Tablename = "location ";
+		var values = "("+
+		location.userid+","+
+		location.area+","+
+		location.ip+","+
+		location.apiKey
+		+")";
+		conDB.query(sql+Tablename+values, function (err, result) {
+		  if (err) throw err;
+		  console.log("Completed The New Endless Loop");
+		  return;
+		});
+		return;
+	});
+}
+
+function createChatsTable(){
+	var conDB = mysql.createConnection(userDB)
+	conDB.connect(function(err) {
+		if (err) { throw err; }
+		console.log("Started A New Endless Loop");
+		var sql = "CREATE TABLE ";
+		var Tablename = "chats ";
+		var values = "("+
+		chats.userid_1+","+
+		chats.userid_2+","+
+		chats.messages_1+","+
+		chats.messages_2
 		+")";
 		conDB.query(sql+Tablename+values, function (err, result) {
 		  if (err) throw err;
